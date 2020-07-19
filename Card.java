d71e6a9afe173ed1ee115c7306e0a6e4b0c8b7af
MrEgggga <anonymous>
2020-07-19 10:06:48 -0700

    Add documentation Add Javadoc pages in doc folder.

--- Card.java
+++ Card.java
@@ -1,20 +1,48 @@
+/**
+ * A class representing a playing card.
+ * @version 1.0
+ * @author MrEgggga
+ * */
 public class Card {
 	private Suit suit;
 	private FaceValue faceValue;
 	
+	/**
+	 * Normal constructor for Cards.
+	 * @param suit The suit of the card
+	 * @param faceValue The face value of the card
+	 * @since 1.0
+	 * */
 	public Card(Suit suit, FaceValue faceValue) {
 		this.suit = suit;
 		this.faceValue = faceValue;
 	}
 	
+	/**
+	 * Get the suit of the card.
+	 * @return The suit of the card
+	 * @since 1.0
+	 * */
 	public Suit getSuit() {
 		return this.suit;
 	}
+	
+	/**
+	 * Get the face value of the card.
+	 * @return The face value of the card
+	 * @since 1.0
+	 * */
 	public FaceValue getFaceValue() {
 		return this.faceValue;
 	}
 	
+	/**
+	 * Get the String representation of the card.
+	 * @return The String representation of the card
+	 * @since 1.0
+	 * */
 	public String toString() {
+		// The face value, then the suit, like 3[diamonds] or T[spades], but with cool Unicode.
 		return this.faceValue.toString() + this.suit.toString();
 	}
 }
--- CardTest.java
+++
@@ -1,9 +0,0 @@
-public class CardTest {
-	public static void main(String[] args) {
-		System.out.println(new Card(Suit.DIAMONDS, FaceValue.TEN));
-		Deck deck = new Deck();
-		System.out.println(deck);
-		deck.shuffle();
-		System.out.println(deck);
-	}
-}
