c907d2d724e0eed4ff3f4922230a20904f8bbee3
MrEgggga <anonymous>
2020-07-19 13:53:49 -0700

    Add makefile Add makefile and remove pre-built documentation. Signed-off-by: MrEgggga <anonymous>

--- StandardValueRanking.java
+++ StandardValueRanking.java
@@ -13,33 +13,19 @@ public class StandardValueRanking {
 	public static int rankingOf(FaceValue faceval) {
 		switch (faceval.toString()) {
 			case "2": 	return 2;
-						break;
 			case "3":	return 3;
-						break;
 			case "4":	return 4;
-						break;
 			case "5":	return 5;
-						break;
 			case "6":	return 6;
-						break;
 			case "7":	return 7;
-						break;
 			case "8":	return 8;
-						break;
 			case "9":	return 9;
-						break;
 			case "T":	return 10;
-						break;
 			case "J":	return 11;
-						break;
 			case "Q":	return 12;
-						break;
 			case "K":	return 13;
-						break;
 			case "A":	return 14;
-						break;
 			default:	return 0;
-						break;
 		}
 	}
 }
